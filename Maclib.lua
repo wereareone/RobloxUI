ea614008badf1046565320c0f9ca4a1a276db0a8
wereareone <wereareone8888@gmail.com>
2026-04-06 15:29:57 +0700

    Update Maclib.lua

--- Maclib.lua
+++ Maclib.lua
@@ -103,25 +103,50 @@ function MacLib:Window(Settings)
 	notificationsUIPadding.PaddingTop = UDim.new(0, 10)
 	notificationsUIPadding.Parent = notifications
 
-	local base = Instance.new("Frame")
-	base.Name = "Base"
-	base.AnchorPoint = Vector2.new(0.5, 0.5)
-	base.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-	base.BackgroundTransparency = Settings.AcrylicBlur and 0.05 or 0
-	base.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	base.BorderSizePixel = 0
-	base.Position = UDim2.fromScale(0.5, 0.5)
-	base.Size = UDim2.fromOffset(868, 550)
-	base.ClipsDescendants = true
+    local base = Instance.new("Frame")
+    base.Name = "Base"
+    base.AnchorPoint = Vector2.new(0.5, 0)
+    base.Position = UDim2.new(0.5, 0, 0.5, -275)
+    base.Size = UDim2.fromOffset(868, 550)
+    
+    base.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+    base.BackgroundTransparency = Settings.AcrylicBlur and 0.05 or 0
+    base.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    base.BorderSizePixel = 0
+    base.ClipsDescendants = true
 
-	local uiConstraint = Instance.new("UISizeConstraint", base)
-	uiConstraint.MaxSize = Vector2.new(1200, 1000) 
-	uiConstraint.MinSize = Vector2.new(500, 100)
+    local uiConstraint = Instance.new("UISizeConstraint", base)
+    uiConstraint.MaxSize = Vector2.new(1200, 1000) 
+    uiConstraint.MinSize = Vector2.new(500, 100)
 
     local baseUICorner = Instance.new("UICorner", base)
     baseUICorner.CornerRadius = UDim.new(0, 12)
 
-	--// BORDER GLOWING GRADIENT
+    local baseUIScale = Instance.new("UIScale", base)
+
+    local function UpdateScale()
+        if not camera then return end
+        local viewportSize = camera.ViewportSize
+        if viewportSize.X < 900 then
+            baseUIScale.Scale = math.clamp(viewportSize.X / 1000, 0.65, 0.9)
+        else
+            baseUIScale.Scale = 1
+        end
+    end
+
+    if camera then
+        camera:GetPropertyChangedSignal("ViewportSize"):Connect(UpdateScale)
+    end
+    UpdateScale() 
+
+    function WindowFunctions:SetScale(Scale)
+        baseUIScale.Scale = Scale
+    end
+    function WindowFunctions:GetScale()
+        return baseUIScale.Scale
+    end
+
+    -- BORDER GLOWING GRADIENT
     local baseUIStroke = Instance.new("UIStroke", base)
     baseUIStroke.Name = "ThemeBorderStroke" 
     baseUIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
@@ -144,7 +169,10 @@ function MacLib:Window(Settings)
         TweenService:Create(borderGradient, spinInfo, {Rotation = 360}):Play()
         TweenService:Create(baseUIStroke, pulseInfo, {Transparency = 0.4}):Play()
     end
-    StartBorderAnimation()
+    
+    if TweenService then
+        StartBorderAnimation()
+    end
 
 	local sidebar = Instance.new("Frame")
 	sidebar.Name = "Sidebar"
